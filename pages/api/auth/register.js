8737260255c753ddc467383ff9f9ef84b98b43f3
MawinWatt <97303992+MawinWatt@users.noreply.github.com>
2022-01-08 00:22:32 +0700

    add files

---
+++ pages/api/auth/register.js
@@ -0,0 +1,35 @@
+import connectDB from "../../../utils/connectDB";
+import Users from '../../../models/userModel'
+import valid from '../../../utils/valid'
+import bcrypt from 'bcrypt'
+
+connectDB()
+
+export default async (req, res) => {
+    switch(req.method){
+        case "POST":
+            await register(req, res)
+            break;
+    }
+}
+
+const register = async (req, res) => {
+    try{
+        const { username, fname, lname, email, password, cf_password } = req.body
+
+        const errMsg = valid(username, fname, lname, email, password, cf_password)
+        if(errMsg) return res.status(400).json({err: errMsg})
+
+        const passwordHash = await bcrypt.hash(password, 12)
+
+        const newUser = new Users({ 
+            username, fname, lname, email, password: passwordHash, cf_password 
+        })
+
+        await newUser.save()
+        res.json({msg: "Register Success!"})
+        
+    }catch(err){
+        return res.status(500).json({err: err.message})
+    }
+}
